696a6da5a1d69241e69e70ae872a5127bb045f72
Jarkko Saltiola <jarkko@saltiolacode.fi>
2023-12-02 19:41:15 +0200

    Disable fancy cmdline

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    --"stevearc/dressing.nvim", enabled = false
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        view = "cmdline",
+      },
+    },
+    --  enabled = false, -- regular cmdline back
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    enabled = true,
+  },
+}
